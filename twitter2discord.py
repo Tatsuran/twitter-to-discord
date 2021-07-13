309f4a4dc812cb96ff93577c345a922a885bb2b2
Tatsuran <tatsuran@gmail.com>
2021-07-14 01:28:24 +0300

    Comments, cleanup (brushed up requests to twitter)

--- twitter2discord.py
+++ twitter2discord.py
@@ -1,48 +1,76 @@
+import json
+
 import aiohttp
 import discord
 from discord.ext import tasks
 
 from tokens import BEARER_TOKEN, DISCORD_TOKEN
-from users import USERS
 
 POLLING_RATE = 1
+CHANNEL_ID = 631474839475060738
+USERS = {
+    'ksononair': '733990222787018753',
+    'KamishiroTaishi': '1261539336278822912'
+}
 
 client = discord.Client()
 
 
-@tasks.loop(seconds=10)  # repeat after every 10 seconds
+# Main loop, repeats every xxx seconds
+@tasks.loop(seconds=10)
 async def main_loop(tweets_posted, channel):
     await check_tweets(tweets_posted, channel)
 
 
+# Action performed after bot is logged in and ready
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
+    # Check channel history and grab id's of tweets that have already been posted
     tweets_posted = []
-    channel = client.get_channel(id=631474839475060738)
+    channel = client.get_channel(id=CHANNEL_ID)
     async for message in channel.history(limit=20):
-        if message.author == client.user:
-            text = message.content
-            if 'twitter' in text and 'status' in text:
-                tweets_posted.append(text.split('status/')[-1])
+        # Ignore messages that are posted not from a bot account
+        if message.author != client.user:
+            continue
+        text = message.content
+        # Ignore bot messages that does not contain tweet links
+        if 'twitter' not in text or 'status' not in text:
+            continue
+        # Grab tweet id and add it to a list of ids
+        tweets_posted.append(text.split('status/')[-1])
     main_loop.start(tweets_posted, channel)
 
 
-async def send(message):
-    channel = client.get_channel(id=631474839475060738)
+# Send message to a specific discord channel
+async def send(message, channel_id):
+    channel = client.get_channel(id=channel_id)
     await channel.send(message)
 
 
+# Parameters for twitter requests, same parameters except max_results
+def params(max_results=5):
+    return {
+        'tweet.fields': 'conversation_id,in_reply_to_user_id',
+        'max_results': max_results
+    }
+
+
+# nothing-here-yet
 async def check_tweets(tweets_posted, channel):
     headers = {'Authorization': f"Bearer {BEARER_TOKEN}"}
     vtuber_id = USERS['ksononair']
     translator_id = USERS['KamishiroTaishi']
-    vtuber_url = f"https://api.twitter.com/2/users/{vtuber_id}/tweets?expansions=author_id,entities.mentions.username,in_reply_to_user_id,referenced_tweets.id,referenced_tweets.id.author_id&tweet.fields=author_id,conversation_id,created_at,in_reply_to_user_id,referenced_tweets,text&user.fields=id,url,name,username&max_results=10"
-    translator_url = f"https://api.twitter.com/2/users/{translator_id}/tweets?expansions=author_id,entities.mentions.username,in_reply_to_user_id,referenced_tweets.id,referenced_tweets.id.author_id&tweet.fields=author_id,conversation_id,created_at,in_reply_to_user_id,referenced_tweets,text&user.fields=id,url,name,username&max_results=50"
+    vtuber_url = f'https://api.twitter.com/2/users/{vtuber_id}/tweets'
+    translator_url = f'https://api.twitter.com/2/users/{translator_id}/tweets'
+
     async with aiohttp.ClientSession() as session:
-        async with session.get(vtuber_url, headers=headers) as responce:
+        async with session.get(vtuber_url, headers=headers, params=params(10)) as responce:
             print(responce.status)
+            print(responce.request_info)
             result = await responce.json()
+            print(json.dumps(result, indent=4, sort_keys=True))
             buffer = []
             for tweet in result['data']:
                 if tweet['id'] in tweets_posted:
@@ -51,10 +79,10 @@ async def check_tweets(tweets_posted, channel):
                 buffer.insert(0, tweet['id'])
             print(buffer)
             for idx in buffer:
-                await send(f'https://twitter.com/ksononair/status/{idx}')
+                await send(f'https://twitter.com/ksononair/status/{idx}', CHANNEL_ID)
                 tweets_posted.append(idx)
 
-        async with session.get(translator_url, headers=headers) as responce:
+        async with session.get(translator_url, headers=headers, params=params(50)) as responce:
             print(responce.status)
             result = await responce.json()
             for tweet in result['data']:
